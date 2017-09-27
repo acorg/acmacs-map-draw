10d8fdbf470acbd51f42ec39ed0fa469c606dedd
Eugene Skepner <github@skepner.eu>
2017-09-27 15:40:17 +0200

    settings summary

--- cc/settings.cc
+++ cc/settings.cc
@@ -32,6 +32,75 @@ static const char* const DEFAULT_SETTINGS_JSON = R"(
 
 // ----------------------------------------------------------------------
 
+// flip
+//   value: [0, 1]
+//   ew
+//   ns
+// rotate_degrees angle:
+// rotate_radians angle:
+// viewport value:
+// use_chart_plot_spec
+// point_scale
+// {"N": "background", "color": "white"},
+// {"N": "grid", "color": "grey80", "line_width": 1},
+// {"N": "border", "color": "black", "line_width": 1},
+// title size: background: border_color: border_width: display_name: []
+// legend data:[{"label": {<label-data>} "display_name": "163-del", "outline": "black", "fill": "#03569b"}] "offset": [-10, -10], "show": True, "size": 50, "label_size": 8, "point_size": 5
+//
+// antigens
+//   select
+//     all
+//     reference
+//     test
+//     passage:
+//       egg
+//       cell
+//       reassortant
+//     name
+//     index
+//     indices []
+//     sequenced
+//     not_sequenced
+//     clade
+//     date_range
+//     older_than_days
+//     younger_than_days
+//     country
+//     continent
+//     vaccine passage: type: (current, previous, surrogate) name:
+//   show
+//   size, shape, fill, outline, outline_width, aspect, rotation, raise_ (order: raise, lower, no-change), report, report_names_threshold
+//   label: name_type, offset, display_name, color, size, weight, slant, font_family
+// {"N": "aa_substitutions", "positions": [159], "legend": {"show": True, "offset": [-10, -10], "point_count": True, "background": "grey99", "border_color": "black", "border_width": 0.1, "label_size": 12, "point_size": 8}},
+// {"N": "aa_substitution_groups", "groups": [{"pos_aa": ["121K", "144K"], "color": "cornflowerblue"}]},
+//
+// sera
+//   select
+//     all
+//     name
+//     index
+//     indices []
+//   size, shape, fill, outline, outline_width, aspect, rotation, raise_ (order: raise, lower, no-change), report, report_names_threshold
+//   label: name_type, offset, display_name, color, size, weight, slant, font_family
+//   serum_circle
+//   serum_coverage
+//
+//  arrow
+//  line
+//  rectangle corner1: corner2:
+//  circle  center: radius: aspect: rotation:
+//
+// Application
+//  lab
+//  labs
+//  lineage
+//
+// Derived:
+//  all-grey
+//  continents
+
+// ----------------------------------------------------------------------
+
 rjson::value default_settings()
 {
     return rjson::parse_string(DEFAULT_SETTINGS_JSON);
