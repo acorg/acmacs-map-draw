38845448ded1e9e0e76c2e0a51e8afd662725105
Eugene Skepner <git@skepner.eu>
2019-03-05 10:29:07 +0100

    map-procrustes improvements

--- cc/map-procrustes.cc
+++ cc/map-procrustes.cc
@@ -1,9 +1,8 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-using namespace std::string_literals;
 
-#include "acmacs-base/argc-argv.hh"
+#include "acmacs-base/argv.hh"
 #include "acmacs-base/read-file.hh"
 #include "acmacs-base/string-split.hh"
 #include "acmacs-base/quicklook.hh"
@@ -15,32 +14,71 @@ using namespace std::string_literals;
 
 // ----------------------------------------------------------------------
 
-static int draw(const argc_argv& args);
+using namespace acmacs::argv;
+struct Options : public argv
+{
+    Options(int a_argc, const char* const a_argv[], on_error on_err = on_error::exit) : argv() { parse(a_argc, a_argv, on_err); }
+
+    option<size_t>    p1{*this, "p1", dflt{0UL}, desc{"projection number of the first chart"}};
+    option<size_t>    p2{*this, "p2", dflt{0UL}, desc{"projection number of the second chart"}};
+    option<str_array> settings{*this, 's'};
+    option<double>    threshold{*this, "threshold", dflt{0.1}, desc{"arrow threshold"}};
+    option<str>       subset{*this, "subset", dflt{"all"}, desc{"all, antigens, sera"}};
+    option<bool>      report{*this, "report", desc{"report common antigens/sera"}};
+    option<bool>      clade{*this, "clade", desc{"color by clade"}};
+    option<str>       viewport{*this, "viewport", dflt{""}, desc{"\"rel\" array of viewport, e.g. 2,2,-4"}};
+    option<bool>      open{*this, "open"};
+
+    argument<str>     chart1{*this, arg_name{"chart"}, mandatory};
+    argument<str>     chart2{*this, arg_name{"secondary-chart"}};
+    argument<str>     pdf{*this, arg_name{"output-pdf"}};
+};
 
 int main(int argc, char* const argv[])
 {
-    int exit_code = 1;
+    int exit_code = 0;
     try {
-        argc_argv args(argc, argv, {
-                {"--p1", 0L, "projection number of the first chart"},
-                {"--p2", 0L, "projection number of the second chart"},
-                {"--threshold", 0.1, "arrow threshold"},
-                {"--subset", "all", "all, antigens, sera"},
-                {"--report", false, "report common antigens/sera"},
-                {"--clade", false},
-                {"--viewport", "", "\"rel\" array of viewport, e.g. 2,2,-4"},
-                {"--open", false},
-                {"--db-dir", ""},
-                {"--time", false, "report time of loading chart"},
-                {"-h", false},
-                {"--help", false},
-                {"--verbose", false},
-                {"-v", false},
-        });
-        if (args["-h"] || args["--help"] || args.number_of_arguments() < 1 || args.number_of_arguments() > 3)
-            std::cerr << "Usage: " << args.program() << " [options] <chart1.ace> [<chart2.ace>] [<pc.pdf>]\n" << args.usage_options() << '\n';
-        else
-            exit_code = draw(args);
+        Options opt(argc, argv);
+        ChartDraw chart_draw(std::make_shared<acmacs::chart::ChartModify>(acmacs::chart::import_from_file(opt.chart1, acmacs::chart::Verify::None, report_time::no)), opt.p1);
+        const std::string secondary_chart((!opt.chart2->empty() && !string::ends_with(*opt.chart2, ".pdf")) ? opt.chart2 : opt.chart1);
+
+        auto settings = settings_default();
+        settings.update(settings_builtin_mods());
+        for (size_t sett_no = 0; sett_no < opt.settings->size(); ++sett_no) {
+            settings.update(rjson::parse_file(opt.settings->at(sett_no), rjson::remove_comments::no));
+        }
+
+        if (!opt.viewport->empty()) {
+            const auto values = acmacs::string::split_into_double(*opt.viewport);
+            settings["apply"].append(rjson::object{{"N", "viewport"}, {"rel", rjson::array(values.begin(), values.end())}});
+        }
+        if (opt.clade) {
+            settings["apply"].append("size_reset");
+            settings["apply"].append("all_grey");
+            settings["apply"].append("egg");
+            settings["apply"].append("clades");
+            settings["apply"].append("vaccines");
+        }
+
+        settings["apply"].append(
+            rjson::object{{"N", "procrustes_arrows"}, {"chart", secondary_chart}, {"projection", *opt.p2}, {"subset", *opt.subset}, {"threshold", *opt.threshold}, {"report", *opt.report}});
+
+        try {
+            apply_mods(chart_draw, settings["apply"], settings);
+        }
+        catch (std::exception& err) {
+            throw std::runtime_error{"Cannot apply " + rjson::to_string(settings["apply"]) + ": " + err.what() + "\n settings:\n" + rjson::pretty(settings, rjson::emacs_indent::no) + '\n'};
+        }
+
+        chart_draw.calculate_viewport();
+
+        const std::string output_file{string::ends_with(*opt.chart2, ".pdf") ? *opt.chart2 : *opt.pdf};
+        acmacs::file::temp temp_file(".pdf");
+        const std::string output = output_file.empty() ? static_cast<std::string>(temp_file) : output_file;
+        chart_draw.draw(output, 800, report_time::yes);
+
+        if (opt.open || output_file.empty())
+            acmacs::quicklook(output, 2);
     }
     catch (std::exception& err) {
         std::cerr << "ERROR: " << err.what() << '\n';
@@ -48,64 +86,6 @@ int main(int argc, char* const argv[])
     }
     return exit_code;
 }
-
-// ----------------------------------------------------------------------
-
-int draw(const argc_argv& args)
-{
-    // const bool verbose = args["-v"] || args["--verbose"];
-    const auto report = do_report_time(args["--time"]);
-    // setup_dbs(args["--db-dir"], verbose);
-    const size_t p1 = args["--p1"], p2 = args["--p2"];
-    const double threshold = args["--threshold"];
-    const bool report_common = args["--report"];
-
-    ChartDraw chart_draw(std::make_shared<acmacs::chart::ChartModify>(acmacs::chart::import_from_file(args[0], acmacs::chart::Verify::None, report)), p1);
-
-    std::string secondary_chart((args.number_of_arguments() > 1 && !string::ends_with(std::string_view(args[1]), ".pdf")) ? args[1] : args[0]);
-    const rjson::value pc{rjson::object{{"N", "procrustes_arrows"},
-                                         {"chart", secondary_chart},
-                                         {"projection", p2},
-                                         {"subset", static_cast<std::string_view>(args["--subset"])},
-                                         {"threshold", threshold},
-                                         {"report", report_common}}};
-
-    rjson::value viewport{rjson::object{{"N", "viewport"}, {"rel", rjson::array{0, 0, 0}}}};
-    if (args["--viewport"]) {
-        const auto values = acmacs::string::split_into_double(args["--viewport"].str());
-        viewport["rel"] = rjson::array(values.begin(), values.end());
-    }
-
-    auto settings = settings_default();
-    settings.update(settings_builtin_mods());
-    if (args["--clade"])
-        settings["apply"] = rjson::array{viewport, "size_reset", "all_grey", "egg", "clades", "vaccines", "title", pc};
-    else
-        settings["apply"] = rjson::array{viewport, "title", pc};
-
-    try {
-        apply_mods(chart_draw, settings["apply"], settings);
-    }
-    catch (std::exception& err) {
-        throw std::runtime_error{"Cannot apply " + rjson::to_string(settings["apply"]) + ": " + err.what() + "\n settings:\n" + rjson::pretty(settings, rjson::emacs_indent::no) + '\n'};
-    }
-
-    chart_draw.calculate_viewport();
-
-    std::string output_file;
-    if (args.number_of_arguments() > 2)
-        output_file = args[2];
-    else if (args.number_of_arguments() > 1 && string::ends_with(std::string_view(args[1]), ".pdf"))
-        output_file = args[1];
-    acmacs::file::temp temp_file(".pdf");
-    const std::string output = output_file.empty() ? static_cast<std::string>(temp_file) : output_file;
-    chart_draw.draw(output, 800, report_time::yes);
-
-    if (args["--open"] || output_file.empty())
-        acmacs::quicklook(output, 2);
-    return 0;
-
-} // draw
 
 // ----------------------------------------------------------------------
 /// Local Variables:
