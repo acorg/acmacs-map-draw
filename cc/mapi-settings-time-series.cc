c1c6a8661c5843ade6790d85b3fff2e9c760a26c
Eugene Skepner <github@skepner.eu>
2020-06-04 09:31:03 +0200

    time series API updated

--- cc/mapi-settings-time-series.cc
+++ cc/mapi-settings-time-series.cc
@@ -39,19 +39,20 @@ bool acmacs::mapi::v1::Settings::apply_time_series()
     if (const auto& end = getenv("end"sv); !end.is_null())
         ts_params.after_last = date::from_string(end.to<std::string_view>(), date::allow_incomplete::yes, date::throw_on_error::yes);
 
-    // const auto ts_stat = acmacs::time_series::stat(ts_params, tal().tree().all_dates());
-    // const auto [first, after_last] = acmacs::time_series::suggest_start_end(ts_params, ts_stat);
+    const auto& chart_access = chart_draw().chart(0); // can draw just the chart 0 // get_chart(getenv("chart"sv), 0);
+    const auto ts_stat = acmacs::time_series::stat(ts_params, chart_access.chart().antigens()->all_dates());
+    const auto [first, after_last] = acmacs::time_series::suggest_start_end(ts_params, ts_stat);
+
     auto series = acmacs::time_series::make(ts_params);
 
-    // if (!ts_stat.counter().empty())
-    //     AD_INFO("time series full range {} .. {}", ts_stat.counter().begin()->first, ts_stat.counter().rbegin()->first);
-    // AD_INFO("time series suggested  {} .. {}", first, after_last);
+    if (!ts_stat.counter().empty())
+        AD_INFO("time series full range {} .. {}", ts_stat.counter().begin()->first, ts_stat.counter().rbegin()->first);
+    AD_INFO("time series suggested  {} .. {}", first, after_last);
     AD_INFO("time series used       {} .. {}", ts_params.first, ts_params.after_last);
     if (rjson::v3::read_bool(getenv("report"sv), false)) {
-        // AD_INFO("time series report:\n{}", ts_stat.report("    {value}  {counter:6d}\n"));
+        AD_INFO("time series report:\n{}", ts_stat.report("    {value}  {counter:6d}\n"));
     }
 
-    const auto& chart_access = chart_draw().chart(0); // can draw just the chart 0 // get_chart(getenv("chart"sv), 0);
     if (const auto filename_pattern = rjson::v3::read_string(getenv("output"sv, toplevel_only::no, throw_if_partial_substitution::no)); filename_pattern.has_value()) {
         auto filename{substitute_chart_metadata(*filename_pattern, chart_access)};
         if (!acmacs::string::endswith_ignore_case(filename, ".pdf"sv))
