e242841ed9b92bc045db51203133b95ea90590c9
Eugene Skepner <github@skepner.eu>
2020-06-10 17:03:10 +0200

    color specification extension ("fill": {...}) to support tal and coloring using time series color scale

--- cc/mapi-settings.hh
+++ cc/mapi-settings.hh
@@ -73,6 +73,8 @@ namespace acmacs::mapi::inline v1
         // ----------------------------------------------------------------------
         // mapi-settings-antigens.cc
 
+        virtual bool apply_antigens();
+        virtual bool apply_sera();
         // returns if key has been processed
         virtual bool select(const acmacs::chart::Antigens& antigens, acmacs::chart::PointIndexList& indexes, std::string_view key, const rjson::v3::value& value) const;
         virtual bool select(const acmacs::chart::Sera& sera, acmacs::chart::PointIndexList& indexes, std::string_view key, const rjson::v3::value& value) const;
@@ -88,8 +90,6 @@ namespace acmacs::mapi::inline v1
         // ----------------------------------------------------------------------
         // mapi-settings-antigens.cc
 
-        bool apply_antigens();
-        bool apply_sera();
         template <typename AgSr> acmacs::chart::PointIndexList select(const AgSr& ag_sr, const rjson::v3::value& select_clause, if_null ifnull) const;
         template <typename AgSr> void check_titrated_against(acmacs::chart::PointIndexList& indexes, std::string_view key, const rjson::v3::value& value) const;
         void mark_serum(size_t serum_index, const rjson::v3::value& style);
