7b4e6b6949675ebffb34220bcda08c38c1cbe124
Eugene Skepner <github@skepner.eu>
2021-06-01 14:24:58 +0200

    point shape in legend

--- cc/map-elements-v1.cc
+++ cc/map-elements-v1.cc
@@ -106,9 +106,8 @@ void map_elements::v1::LegendPointLabel::draw(acmacs::draw::DrawElements& aDrawE
             .border_color(mBorderColor)
             .border_width(mBorderWidth);
     legend.interline(mInterline);
-    for (const auto& line : mLines) {
-        legend.add(line.label, mLabelColor, mLabelSize, mLabelStyle, mPointSize, line.outline, line.outline_width, line.fill, acmacs::PointShape::Circle);
-    }
+    for (const auto& line : mLines)
+        legend.add(line.label, mLabelColor, mLabelSize, mLabelStyle, mPointSize, line.outline, line.outline_width, line.fill, line.shape_);
 
 } // map_elements::v1::LegendPointLabel::draw
 
