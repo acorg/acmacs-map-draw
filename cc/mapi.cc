9101eb3b508f0f2d7f9ba5e932b57e5401a8c530
Eugene Skepner <github@skepner.eu>
2021-07-16 11:49:25 +0200

    clade keys fixed to support mapi, tal, chains202105

--- cc/mapi.cc
+++ cc/mapi.cc
@@ -77,7 +77,7 @@ int main(int argc, char* const argv[])
 
         ChartDraw chart_draw{inputs, opt.projection};
         auto& settings = chart_draw.settings();
-        settings.load_from_conf({"mapi.json"sv, "clades.json"sv, "vaccines.json"sv});
+        settings.load_from_conf({"mapi.json"sv, "clades.mapi"sv, "vaccines.json"sv});
         settings.load(opt.settings_files);
         settings.set_defines(opt.defines);
         // for (size_t chart_no = 0; chart_no < chart_draw.number_of_charts(); ++chart_no) {
